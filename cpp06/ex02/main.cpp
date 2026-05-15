3bc7ae9f60b4ae841b3e3ef2833027049755369c
Megamax21 <lhotemaxime@hotmail.com>
2026-05-15 03:12:09 +0200

    Done with cpp06

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -1,26 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ml-hote <ml-hote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/14 19:47:43 by ml-hote           #+#    #+#             */
-/*   Updated: 2026/05/15 03:04:16 by ml-hote          ###   ########.fr       */
+/*   Created: 2026/05/15 02:26:15 by ml-hote           #+#    #+#             */
+/*   Updated: 2026/05/15 03:09:35 by ml-hote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILS_HPP
-#define UTILS_HPP
+#include "utils.hpp"
 
-#include "A.hpp"
-#include "B.hpp"
-#include "C.hpp"
-#include "Base.hpp"
-#include <exception>
-
-Base *	generate(void);
-void	identify(Base* p);
-void	identify(Base &p);
-
-#endif
+int main(void)
+{
+	srand(time(0));
+	Base* base1;
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "=============" << std::endl;
+		base1 = generate();
+		identify(base1);
+		identify(*base1);
+	}
+	return (0);
+}
--- cpp06/ex02/utils.cpp
+++ cpp06/ex02/utils.cpp
@@ -6,17 +6,66 @@
 /*   By: ml-hote <ml-hote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 19:47:24 by ml-hote           #+#    #+#             */
-/*   Updated: 2026/05/14 20:13:28 by ml-hote          ###   ########.fr       */
+/*   Updated: 2026/05/15 03:11:14 by ml-hote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.hpp"
 
-// srand(time(0));
-//     for (int i = 0; i < 3; i++)
-//         cout << rand() << endl;
+void identify(Base* p)
+{
+	A* a1 = (dynamic_cast<A*>(p));
+	B* b1 = (dynamic_cast<B*>(p));
+	C* c1 = (dynamic_cast<C*>(p));
+	std::cout << "Base *p : ";
+	if (a1)
+		std::cout << "A" << std::endl;
+	if (b1)
+		std::cout << "B" << std::endl;
+	if (c1)
+		std::cout << "C" << std::endl;
+}
+/* What is catch(...) ?
+	The catch (...) handler matches exceptions of any type. If present,
+	it can only be the last handler in a handler sequence.
+*/
+void identify(Base &p)
+{
+	std::cout << "Base &p : ";
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+		return;
+	}
+	catch (...) {}
+	try
+	{
+		(void)dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+		return;
+	}
+	catch (...) {}
+	try
+	{
+		(void)dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+		return;
+	}
+	catch (...) {}
+}
 
 Base*	generate(void)
 {
-	
+	switch (rand()%3)
+	{
+	case 0:
+		return (new A);
+	case 1:
+		return (new B);
+	case 2:
+		return (new C);
+	}
+	return (new Base);
 }
+
